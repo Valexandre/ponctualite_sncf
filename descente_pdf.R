937562027c6fa564d34a54a8574f90bfcd347769
Victor <Valexandre@users.noreply.github.com>
2024-01-01 14:30:16 +0100

    Update descente_pdf.R

--- descente_pdf.R
+++ descente_pdf.R
@@ -26,6 +26,71 @@ alldispo<-list.files("data/",pattern="pdf")
 nouveauxdispo<-alldispo[!alldispo%in%fichiersdejadispo]
 
 
+SorsLesDonneesDesPDF<-function(urlascrap){
+  gooddate<-gsub("\\.pdf","",gsub("%20"," ",urlascrap))
+  PDF <- pdf_text(paste0("data/",urlascrap)) %>%
+    readr::read_lines() #open 
+  debutdata<-which(grepl("AXE PARIS - ROUEN - LE HAVRE",PDF))
+  
+  PDFDF<-PDF[debutdata:length(PDF)] %>%
+    str_squish() %>%
+    as.data.frame(.)
+  
+  colnames(PDFDF)<-"brut"
+  PDFDF$EstuneHeure<-ifelse(grepl("\\d\\dh\\d\\d",substr(PDFDF$brut,1,5)),"heure","non")
+  
+  PDFDF<-PDFDF%>%
+    mutate(Depart=case_when(EstuneHeure=="heure"~lag(brut),TRUE~""),
+           Arrivee=case_when(EstuneHeure=="heure"~gsub("\\d\\dh\\d\\d (.+) \\d\\dh\\d\\d","\\1",brut), TRUE~""),
+           HeureDepart=case_when(EstuneHeure=="heure"~substr(brut,1,5),TRUE~""),
+           HeureArrivee=case_when(EstuneHeure=="heure"~gsub("\\d\\dh\\d\\d (.+) (\\d\\dh\\d\\d)","\\2",brut),      TRUE~""),
+           ALheure=case_when(EstuneHeure=="heure" & grepl("A l'heure",brut)~"A l'heure",
+                             EstuneHeure=="heure" & grepl("Supprimé",lag(brut))~"Supprimé",
+                             EstuneHeure=="heure" & grepl("Supprimé",brut)~"Supprimé",
+                             EstuneHeure=="heure" & grepl("Supp",lag(brut))~"Supp partielle",
+                             TRUE~"Non"),
+           Retard=case_when(EstuneHeure=="heure" & grepl("min ",brut)~as.numeric(gsub("\\+","",gsub("\\d\\dh\\d\\d (.+) min (.+)","\\1",HeureArrivee))),TRUE~0),
+           MessagePart1=gsub("ROUEN RIVE DROITE","",gsub("PARIS ST-LAZARE","",gsub("PARIS-MONTPARNASSE","",gsub("ARGENTAN","",gsub("PARIS-VAUGIRARD","",gsub("GRANVILLE","",gsub("SERQUIGNY","",gsub("VERNON-GIVERNY","",gsub("ÉVREUX-NORMANDIE","",gsub("TROUVILLE-DEAUVILLE","",gsub("CAEN","",gsub("CHERBOURG","",gsub("PARIS-ST-LAZARE","",gsub("LE HAVRE","",gsub("ROUEN-RIVE-DROITE","",Depart))))))))))))))),
+           MessagePart2=gsub("\\d\\dh\\d\\d (.+) min (.+)","\\2",HeureArrivee),
+           EnsembleMessage=ifelse(nchar(MessagePart1)<2,"",str_trim(paste0(MessagePart1," ",MessagePart2))),
+           Depart=case_when(grepl("ÉVREUX-NORMANDIE",Depart)~"ÉVREUX-NORMANDIE",
+                            grepl("TROUVILLE-DEAUVILLE",Depart)~"TROUVILLE-DEAUVILLE",
+                            grepl("CAEN",Depart)~"CAEN",
+                            grepl("PARIS-MONTPARNASSE",Depart)~"PARIS-MONTPARNASSE",
+                            grepl("SERQUIGNY",Depart)~"SERQUIGNY",
+                            grepl("PARIS-VAUGIRARD",Depart)~"PARIS-VAUGIRARD",
+                            grepl("ARGENTAN",Depart)~"ARGENTAN",
+                            grepl("GRANVILLE",Depart)~"GRANVILLE",
+                            grepl("VERNON-GIVERNY",Depart)~"VERNON-GIVERNY",
+                            grepl("CHERBOURG",Depart)~"CHERBOURG",
+                            grepl("PARIS-ST-LAZARE",Depart)~"PARIS-ST-LAZARE",
+                            grepl("PARIS ST-LAZARE",Depart)~"PARIS-ST-LAZARE",
+                            grepl("LE HAVRE",Depart)~"LE HAVRE",
+                            grepl("ROUEN-RIVE-DROITE",Depart)~"ROUEN-RIVE-DROITE",
+                            grepl("ROUEN RIVE DROITE",Depart)~"ROUEN-RIVE-DROITE"),
+           Arrivee=case_when(grepl("ÉVREUX-NORMANDIE",Arrivee)~"ÉVREUX-NORMANDIE",
+                             grepl("TROUVILLE-DEAUVILLE",Arrivee)~"TROUVILLE-DEAUVILLE",
+                             grepl("CAEN",Arrivee)~"CAEN",
+                             grepl("PARIS-MONTPARNASSE",Arrivee)~"PARIS-MONTPARNASSE",
+                             grepl("SERQUIGNY",Arrivee)~"SERQUIGNY",
+                             grepl("PARIS-VAUGIRARD",Arrivee)~"PARIS-VAUGIRARD",
+                             grepl("ARGENTAN",Arrivee)~"ARGENTAN",
+                             grepl("GRANVILLE",Arrivee)~"GRANVILLE",
+                             grepl("VERNON-GIVERNY",Arrivee)~"VERNON-GIVERNY",
+                             grepl("CHERBOURG",Arrivee)~"CHERBOURG",
+                             grepl("PARIS-ST-LAZARE",Arrivee)~"PARIS-ST-LAZARE",
+                             grepl("PARIS ST-LAZARE",Arrivee)~"PARIS-ST-LAZARE",
+                             grepl("LE HAVRE",Arrivee)~"LE HAVRE",
+                             grepl("ROUEN-RIVE-DROITE",Arrivee)~"ROUEN-RIVE-DROITE",
+                             grepl("ROUEN RIVE DROITE",Arrivee)~"ROUEN-RIVE-DROITE"),
+           HeureArrivee=substr(HeureArrivee,1,5))%>%
+    filter(!is.na(Depart))
+  
+  Resultat<-PDFDF%>%select(Depart,Arrivee,HeureDepart,HeureArrivee,ALheure,Retard,EnsembleMessage)%>%
+    mutate(date=gooddate)
+  return(Resultat)
+}
+
 # On crée le tableur avec les pdf
 TestDF<-nouveauxdispo[1]%>%map_dfr(SorsLesDonneesDesPDF)
 TestSors<-possibly(SorsLesDonneesDesPDF,otherwise = TestDF[1:5,])
